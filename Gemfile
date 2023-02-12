21fa8ad3f680901fcd071465462224ec62fceb60
JordanJoseph <jordant.joseph@outlook.com>
2023-02-12 10:22:12 -0700

    install gems, ignore mapbox key, create method to seed from API

--- Gemfile
+++ Gemfile
@@ -12,9 +12,6 @@ gem "pg", "~> 1.1"
 # Use the Puma web server [https://github.com/puma/puma]
 gem "puma", "~> 5.0"
 
-# Use Active Model has_secure_password
-gem 'bcrypt', '~> 3.1.7'
-
 # Build JSON APIs with ease [https://github.com/rails/jbuilder]
 # gem "jbuilder"
 
@@ -24,8 +21,12 @@ gem 'bcrypt', '~> 3.1.7'
 # Use Kredis to get higher-level data types in Redis [https://github.com/rails/kredis]
 # gem "kredis"
 
+gem "rest-client"
+
+gem "dotenv-rails" 
+
 # Use Active Model has_secure_password [https://guides.rubyonrails.org/active_model_basics.html#securepassword]
-# gem "bcrypt", "~> 3.1.7"
+gem "bcrypt", "~> 3.1.7"
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem "tzinfo-data", platforms: %i[ mingw mswin x64_mingw jruby ]
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,28 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
+require 'rest-client'
+
 
 u1 = User.create(username: "Admin", password: "12345")
+
+puts "Getting Ski Data"
+
+def ski_data 
+    ski = RestClient.get("https://skimap.org/SkiAreas/.json")
+    ski_array = JSON.parse(ski)
+    ski_array.each do |s|
+        Resort.create(
+            name: s["name"],
+            website: s["official_website"],
+            terrain_park: s["terrain_park"],
+            night_skiing: s["night_skiing"],
+            operating_status: s["operating_status"],
+            latitude: s["latitude"],
+            longitude: s["longitude"]
+        )
+    end
+end
+
 
 puts 'seeded!'
