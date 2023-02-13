ca90979fd05fadbafe80a7345fd0cf1a09ae3a0e
JordanJoseph <jordant.joseph@outlook.com>
2023-02-13 10:23:07 -0700

    create Resorts.jsx, fetch for data and pass props

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,26 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Home from './components/Home'
+import Resorts from './components/Resorts'
 import {UserProvider} from './context/user';
 
 function App() {
 
+  const [resorts, setResorts] = useState([]);
 
+  useEffect(() => {
+    fetch('./resorts/show')
+    .then(r => r.json())
+    .then(r => setResorts(r))
+  }, []);
 
   return (
     <UserProvider>
       <Router>
         <Routes>
           <Route path='/' element={<Home/>}/>
+          <Route path='/resort-maps' element={<Resorts resorts={resorts}/>}/>
         </Routes>
       </Router>
     </UserProvider>
---
+++ client/src/components/Resorts.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Resorts = ({resorts}) => {
+
+    console.log('are resorts coming in ??', resorts);
+    return(
+        <p>Some shit here</p>
+    )
+}
+
+export default Resorts;
