094892e0980d3b8ce0a78fbd720eaacd2bf7bcdb
JordanJoseph <jordant.joseph@outlook.com>
2023-02-20 15:50:59 -0700

    import and route navbar in App, add MUI navbar component and label buttons for resorts, bookmarks, login/logout and signup

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React, {useEffect, useState} from 'react';
 import './App.css';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Home from './components/Home';
+import Navbar from './components/Navbar';
 import Login from './components/Login';
 import Signup from './components/Signup';
 import Resorts from './components/Resorts';
@@ -20,11 +21,12 @@ function App() {
   return (
     <UserProvider>
       <Router>
+        <Navbar/>
         <Routes>
           <Route path='/' element={<Home/>}/>
           <Route path='/login' element={<Login/>}/>
           <Route path='/signup' element={<Signup/>}/> 
-          <Route path='/resort-maps' element={<Resorts resorts={resorts}/>}/>
+          <Route path='/resort-map' element={<Resorts resorts={resorts}/>}/>
         </Routes>
       </Router>
     </UserProvider>
---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,75 @@
+import React, {useContext} from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import Button from '@mui/material/Button';
+import {Link} from 'react-router-dom';
+import { UserContext } from "../context/user";
+
+const Navbar = () => {
+
+    const user = useContext(UserContext);
+
+    return(
+        <AppBar position="static">
+        <Container maxWidth="xl">
+          <Toolbar disableGutters>
+            <Typography
+              variant="h6"
+              component={Link} to=''
+              sx={{
+                mr: 2,
+                display: { xs: 'none', md: 'flex' },
+                fontFamily: 'Arial',
+                fontWeight: 700,
+                letterSpacing: '.3rem',
+                color: 'inherit',
+                textDecoration: 'none',
+              }}
+            >
+              Ski
+            </Typography>
+            <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+              <Button
+                  sx={{ my: 2, color: 'white', display: 'block' }}
+                  component={Link} to='/resort-map'
+              >Resorts
+              </Button>
+              <Button
+                  sx={{ my: 2, color: 'white', display: 'block' }}
+                  component={Link} to='/aplaceihavetomakestill'
+              >Bookmarked Resorts
+              </Button> 
+            </Box>
+            { user ? (
+              <Button
+                  //onClick={handleLogout}
+                  sx={{ my: 2, color: 'white', display: 'block' }}
+              > Logout
+              </Button>
+              ) : (
+                <Button
+                component={Link} to='/login'
+                sx={{ my: 2, color: 'white', display: 'block' }}
+            > Login
+            </Button>
+              )}
+              {!user ? (
+              <Button
+                  component={Link} to='/signup'
+                  sx={{ my: 2, color: 'white', display: 'block' }}
+              >
+              Signup
+              </Button>
+              ) : <></>}
+            <Box sx={{ flexGrow: 0 }}>
+            </Box>
+          </Toolbar>
+        </Container>
+      </AppBar>
+    );
+}
+
+export default Navbar;
