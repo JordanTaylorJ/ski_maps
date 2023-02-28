9bbb2d9fcf93cfe28ccfd7d6da5efcf9e9d241d8
JordanJoseph <jordant.joseph@outlook.com>
2023-02-27 19:05:35 -0700

    add user context to bookmarks, map through the user notes and render

--- client/src/components/Bookmarks.jsx
+++ client/src/components/Bookmarks.jsx
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, {useContext, useState} from 'react';
+import { UserContext } from "../context/user";
 
 const Bookmarks = () => {
 
+    const {user, setUser} = useContext(UserContext);
+    const [bookmarks, setBookmarks] = useState(user.bookmarks)
+
+
     return(
-        <h1>Bookmarks here</h1>
+        <>
+        {bookmarks.map(bookmark => 
+            <p>{bookmark.notes} here</p>
+        )} 
+        </>
     )
 }
 
