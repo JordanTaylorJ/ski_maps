31b0efacfcac4a4aec804088efc1751c03c1514e
JordanJoseph <jordant.joseph@outlook.com>
2023-02-28 10:51:50 -0700

    rails resource generator for comments, migrate table, comment out routes until needed

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
-  resources :bookmarks
-  resources :resorts, only: [:show]
-  resources :users, only: [:show, :create]
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  #resources :comments
+  #resources :bookmarks
+  resources :resorts, only: [:show]
+  resources :users, only: [:show, :create]
 
   post '/signup', to: 'users#create'
   get '/auth', to: 'users#show'
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_12_174745) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_28_174529) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "bookmarks", force: :cascade do |t|
     t.string "notes"
+    t.integer "user_id"
+    t.integer "resort_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.string "comment"
     t.integer "user_id"
     t.integer "resort_id"
     t.datetime "created_at", null: false
