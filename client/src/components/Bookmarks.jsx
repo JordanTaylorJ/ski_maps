476032c124910026d795f46eefdac11b98aa9cc5
JordanJoseph <jordant.joseph@outlook.com>
2023-03-02 11:56:51 -0700

    use a similar toggle as the comments to render editing for Bookmarks, keep an edit id in state and render bookmarks based on their ids, define handleCancelEditClick to exit the edit form

--- client/src/components/Bookmarks.jsx
+++ client/src/components/Bookmarks.jsx
@@ -1,24 +1,76 @@
 import React, {useContext, useState} from 'react';
 import { UserContext } from "../context/user";
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 
 const Bookmarks = () => {
 
     const {user, setUser} = useContext(UserContext);
     const [bookmarks, setBookmarks] = useState(user.bookmarks)
+    const [editBookmarkId, setEditBookmarkId] = useState(null)
+    const [editBookmark, setEditBookmark] = useState({
+        resort_id: '',
+        user_id: user.id,
+        notes: ''
+    })
+
+    const handleCancelEditClick = () => {
+        setEditBookmarkId(null);
+    }
 
     return(
-        <>
-        {bookmarks.map(bookmark => 
-            <div>
+        <div>
+        {bookmarks.map(bookmark => {
+            if (bookmark.id === editBookmarkId){
+                return(
+                <Box
+                    key={bookmark.id}
+                    component="form"
+                    //onSubmit={(e) => handleSubmitEdit(e)}
+                    sx={{
+                    '& > :not(style)': { m: 1, width: '25ch' },
+                    }}
+                    noValidate
+                    autoComplete="off"
+                >
                 <h2>{bookmark.resort.name}</h2>
-                <p>Notes: {bookmark.notes} here</p>
-                <Button>
-                    Edit
+                <TextField 
+                    id="outlined-multiline-flexible"
+                    multiline
+                    maxRows={4}
+                    label="Notes" 
+                    variant="standard" 
+                    type='text'
+                    name='notes'
+                    value={editBookmark.notes}
+                    //onChange={(e) => handleEditFormChange(e)} 
+                />
+                <Button 
+                    type='submit'
+                > Save 
                 </Button>
-            </div>
+                <Button
+                    onClick={handleCancelEditClick}
+                > Cancel 
+                </Button>
+            </Box>
+                )
+            } else {
+                return(
+                <div key={bookmark.id} >
+                    <h2>{bookmark.resort.name}</h2>
+                    <p>Notes: {bookmark.notes} here</p>
+                    <Button onClick={() => setEditBookmarkId(bookmark.id)}
+                    >
+                        Edit
+                    </Button>
+                </div>
+                )
+            }
+        } 
         )} 
-        </>
+        </div>
     )
 }
 
