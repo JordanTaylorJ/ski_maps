2dc5322ed67217c46164761189fd86def3d288f6
JordanJoseph <jordant.joseph@outlook.com>
2023-03-12 22:38:29 -0600

    edit build file for render deployment, route to fallback_controller for html

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,17 @@
 Rails.application.routes.draw do
   
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  namespace :api do
+    resources :comments, only: [:create, :update, :destroy]
+    resources :bookmarks, only: [:create, :destroy]
+    resources :resorts, only: [:show]
+    resources :users, only: [:show, :create]
 
-  # Defines the root path route ("/")
-  # root "articles#index"
-
-  resources :comments, only: [:create, :update, :destroy]
-  resources :bookmarks, only: [:create, :destroy]
-  resources :resorts, only: [:show]
-  resources :users, only: [:show, :create]
-
-  post '/signup', to: 'users#create'
-  get '/auth', to: 'users#show'
-  post '/login', to: 'sessions#create'
-  delete '/logout', to: 'sessions#destroy'
+    post '/signup', to: 'users#create'
+    get '/auth', to: 'users#show'
+    post '/login', to: 'sessions#create'
+    delete '/logout', to: 'sessions#destroy'
+  end 
   
+  get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
+
 end
