57d11d48efc84fc224cd27a4286b00404af4d463
JordanJoseph <jordant.joseph@outlook.com>
2023-03-13 13:46:29 -0600

    make username validation not case sensitive

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
     has_many :resorts, through: :comments 
 
     has_secure_password  
-    validates :username, presence: true, uniqueness: true 
+    validates :username, presence: true, uniqueness: { case_sensitive: false }
     validates :password, presence: true, length: {in: 5..20}
 
 end
