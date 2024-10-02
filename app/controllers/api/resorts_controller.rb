f24ac5fcbe47b8f41f52321f2a385b169dcb9700
JordanJoseph <jordant.joseph@outlook.com>
2024-10-02 07:53:56 -0600

    create custom route to filter database resorts, define method in resorts controller, create checkbox on the client with fetch request to pass filter parameters to the database

--- app/controllers/api/resorts_controller.rb
+++ app/controllers/api/resorts_controller.rb
@@ -1,10 +1,22 @@
 class Api::ResortsController < ApplicationController
 
-    skip_before_action :authorized, only: [:show] 
+    skip_before_action :authorized, only: [:show, :filtered] 
 
     def show
         resorts = Resort.all
         render json: resorts, include: 'comments.user', status: :ok
     end
+
+    def filtered 
+        data = JSON.parse(params[:data])
+        filtered_resorts = Resort.all 
+        data.each do |key, value| 
+            if value == true
+                filtered_resorts = filtered_resorts.filter {|resort| resort[key] == true}
+            end
+        end 
+        render json: filtered_resorts, status: :ok 
+    end 
+
 
 end
--- client/src/components/ResortFilter.jsx
+++ client/src/components/ResortFilter.jsx
@@ -7,28 +7,25 @@ import FormGroup from '@mui/material/FormGroup';
 const ResortFilter = ({resorts}) => {
 
     const [filteredResorts, setFilteredResorts] = useState(resorts)
-    const [filters, setFilters] = useState({terrain: false, night: false})
-    
-    const handleChange = (e) => {
-        setFilters({...filters, [e.target.name]:e.target.checked})
-        fetch(`/api/resorts/${filters.toString()}`) 
-        .then(r => r.json())
-        .then(data => setFilteredResorts(data))
-    }
-    console.log('what is changing', filters)
-    console.log('the resorts filtered', filteredResorts)
+    const [filters, setFilters] = useState({terrain_park: false, night_skiing: false})
 
-    const handleSubmit = (e) => {
-        //
-        e.preventDefault(); 
-        fetch(`/api/resorts/${filters}`) 
+    const handleChange = (e) => {   
+        setFilters({...filters, [e.target.name]: e.target.checked})
+        //handleSubmit(filters)
+    }
+
+    const handleSubmit = (filters) => {
+        const params = encodeURIComponent(JSON.stringify(filters))
+        console.log('params', params)
+        fetch(`/api/resorts/filters?data=${params}`) 
         .then(r => r.json())
+        //.then(data => console.log('returned', data))
         .then(data => setFilteredResorts(data))
     }
 
     return(
         <Box
-            height={100}
+            height={100}s
             width={200}
             m={4}
             alignItems="center"
@@ -38,12 +35,57 @@ const ResortFilter = ({resorts}) => {
             sx={{ border: '2px solid grey' }}
         >
             <FormGroup>
-                <FormControlLabel control={<Checkbox value={filters.terrain} name='terrain' onChange={(e) => handleChange(e)}></Checkbox>} label='Terrain Park' />
-                <FormControlLabel control={<Checkbox value={filters.night} name='night' onChange={(e) => handleChange(e)} ></Checkbox>} label='Night Skiing' />
-            </FormGroup>    
+                <FormControlLabel control={<input type='checkbox' checked={filters.terrain_park} name='terrain_park' onChange={handleChange}></input>} label='Terrain Park' />
+                <FormControlLabel control={<input type='checkbox' checked={filters.night_skiing} name='night_skiing' onChange={handleChange} ></input>} label='Night Skiing' />
+            </FormGroup>  
         </Box>
     )
-
 }
 
 export default ResortFilter;
+
+/*
+             console.log('what type', typeof filters.terrain)
+
+            <input type='checkbox' checked={false} value='terrain' onChange={handleChange} />
+            
+
+            
+
+            if (e.target.checked){
+            setFilters([...filters, e.target.value])
+        } else {
+            setFilters(filters.filter((item) => item !== e.target.value))
+        }
+
+
+
+            console.log('what', typeof filters.terrain)
+    const handleChange = (e) => {
+        
+        setFilters({...filters, [e.target.name]: e.target.checked})
+        console.log('what happends here', filters)
+        //handleSubmit(filters)
+
+
+                
+        console.log('what happends here', filters)
+        handleSubmit(filters)
+
+        {terrain: true, night: false}
+
+
+
+                if (e.target.checked){
+            setFilters([...filters, e.target.value])
+        } else {
+            setFilters(filters.filter((item) => item !== e.target.value))
+        }
+
+
+                   <FormGroup>
+                <FormControlLabel control={<input type='checkbox' checked={filters.terrain_park} name='terrain_park' onChange={handleChange}></input>} label='Terrain Park' />
+                <FormControlLabel control={<input type='checkbox' checked={filters.night_skiing} name='night_skiing' onChange={handleChange} ></input>} label='Night Skiing' />
+            </FormGroup>  
+
+            */
